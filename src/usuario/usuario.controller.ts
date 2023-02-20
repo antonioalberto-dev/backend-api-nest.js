b387002cc386fd767cf6513bb1c26cc5ea2060bd
Antonio Alberto <antonioalbertodsb@gmail.com>
2023-02-20 16:45:22 -0400

    feat: adicionando validações aos campos

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { Get } from "@nestjs/common/decorators";
+import { CriaUsuarioDTO } from "./dto/CriaUsuario";
 import { UsuarioRepository } from "./usuario.repository";
 
 @Controller('/usuarios')
@@ -8,7 +9,7 @@ export class UsuarioController {
     constructor(private usuarioRepository: UsuarioRepository) { }
 
     @Post()
-    async criaUsuario(@Body() dadosDoUsuario) {
+    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
         this.usuarioRepository.salvar(dadosDoUsuario)
         return dadosDoUsuario;
     }
